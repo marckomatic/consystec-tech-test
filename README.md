f7472749b5cc926cc2100b725c985d7adfa11ad7
Marco Mazariegos <marcomazariegos18@gmail.com>
2025-04-12 23:56:38 -0600

    Added readme and forgotter corsconfig and odcker compose

---
+++ README.md
@@ -0,0 +1,29 @@
+# Notecs App 
+
+Aplicacion web para crear y gestionar tareas para la prueba Tecnica de Consystec. Esta app usa Angular 19 en el frontend y Spring Boot 3 con Gradle 8.5 y Java 21 en el backend.
+Utiliza Amazon RDS para la base de datos, docker para gestionar las aplicaciones y una instancia EC2 para tener los containers de las dos apps.
+---
+
+## Ariquitectura
+
+![Vista previa de la app](./arquitectura-notecs.drawio.png)
+
+---
+
+## Tecnologías utilizadas
+
+- Angular 19
+- Node.js 22.1.4
+- Spring Boot 3
+- Gradle 8.5
+- Java 21
+- MySQL
+- Amazon RDS
+- EC2
+- Docker
+
+
+
+
+
+
---
+++ notecs-app/src/main/java/com/notecs/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.notecs.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // Configura CORS para todas las rutas ("/**")
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
